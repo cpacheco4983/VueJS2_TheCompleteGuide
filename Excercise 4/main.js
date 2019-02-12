356cdf5ccf74ee578822327e60caa2d731abebf3
Carlos Pacheco <GAS01661@gassouth.com>
2019-02-12 16:03:44 -0500

    Completed exercise 4

--- Excercise 4/main.js
+++ Excercise 4/main.js
@@ -1,11 +1,39 @@
 new Vue({
   el: '#exercise',
   data: {
-
+    effectClasses: {
+      highlight: false,
+      shrink: true,
+      'hi-contrast': false,
+      userClass: '',
+      userClass2: ''
+    },
+    styleClass: {
+      width: '100px',
+      height: '100px',
+      'background-color': 'green'
+    },
+    isVisible: false,
+    progressBar: {
+      width: 0,
+      'background-color': 'blue'
+    }
   },
   methods: {
     startEffect: function() {
+        var vObj = this;
+        setInterval(function() {
+          vObj.effectClasses.highlight = !vObj.effectClasses.highlight;
+          vObj.effectClasses.shrink = !vObj.effectClasses.shrink;
+        }, 1000);
+    },
+    startProgress: function() {
+      var vObj = this,
+          progress = 0;
 
+      setInterval(function() {
+        vObj.progressBar.width = progress++ + 'px';
+      }, 1000);
     }
   }
 });
